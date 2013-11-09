43e32a4c613444826bda87d0f19152a316bd7642
Michal Kuklis <michal.kuklis@gmail.com>
2013-11-09 16:00:47 -0500

    Issue #24 - Changed default draft to draft 17

--- src/com/ququplay/websocket/WebSocket.java
+++ src/com/ququplay/websocket/WebSocket.java
@@ -10,7 +10,7 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.java_websocket.drafts.Draft;
-import org.java_websocket.drafts.Draft_10;
+import org.java_websocket.drafts.Draft_17;
 import org.java_websocket.WebSocketImpl;
 
 import org.json.JSONArray;
@@ -114,7 +114,7 @@ public class WebSocket extends CordovaPlugin {
   private Draft getDraft(JSONObject options, CallbackContext callbackContext) {
 
     String draftName;
-    Draft draft = new Draft_10();
+    Draft draft = new Draft_17();
         
     try {
       draftName = options.getString("draft");
